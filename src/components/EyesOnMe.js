78571214f53a42d0b6ef8f81c51cf14640102b82
Alex Cline <rev.a.r.cline@gmail.com>
2021-04-14 13:34:54 -0500

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,15 @@
-// Code EyesOnMe Component Here
+import React from "react";
+
+class Keypad extends React.Component {
+  passwordWatcher = () => console.log("Entering password...");
+
+  render() {
+    return (
+      <div>
+        <input type="password" onKeyUp={this.passwordWatcher} />
+      </div>
+    );
+  }
+}
+
+export default Keypad;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,17 @@
-// Code Keypad Component Here
+import React from "react";
+
+class EyesOnMe extends React.Component {
+  focusButton = () => console.log("Good!");
+
+  blurButton = () => console.log("Hey! Eyes on me!");
+
+  render() {
+    return (
+      <button onFocus={this.focusButton} onBlur={this.blurButton}>
+        Eyes on me, please!
+      </button>
+    );
+  }
+}
+
+export default EyesOnMe;
